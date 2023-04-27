716c57b1d144d8327906de02a79b01c869678a46
Anna Nasto <anna.nasto18@gmail.com>
2023-04-26 21:43:28 -0500

    more changes to formSubmission function

--- scriptHelper.js
+++ scriptHelper.js
@@ -79,12 +79,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     fuelStatus.innerHTML = "Fuel level too low for launch";
     cargoStatus.innerHTML = "Cargo mass too heavy for launch";
     launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+    launchStatus.style.color = "rgb(199, 37, 78)";
   } else {
     list.style.visibility = "visible";
     cargoStatus.innerHTML = "Cargo mass low enough for launch";
     fuelStatus.innerHTML = "Fuel level high enough for launch";
     launchStatus.innerHTML = "Shuttle is Ready for Launch";
-    launchStatus.style.color = "green";
+    launchStatus.style.color = "rgb(65, 159, 106)";
   }
 }
 
